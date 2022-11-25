6401273a45a2eec08c5557e3c95131ee37aa736b
Daniel Louis <it_canada@hotmail.com>
2022-11-24 21:24:36 -0500

    lesson 09

--- errorhandling-lesson-07/src/main/scala/com/newhope/fortune/application/SomeNoneDemo.scala
+++ errorhandling-lesson-07/src/main/scala/com/newhope/fortune/application/SomeNoneDemo.scala
@@ -1,5 +1,44 @@
 package com.newhope.fortune.application
 
-class SomeNoneDemo {
+object SomeNoneDemo extends App {
+    //   Some, None, try catch demo
+    def toInt(s: String): Option[Int] = {
+      try {
+        Some(Integer.parseInt(s.trim))
+      } catch {
+        case e: Exception => None
+      }
+    }
+
+    x
+    // CALL above toInt function defined aboves
+    val x: String = "10"
+    val result = toInt(x) match {
+      case Some(i) => i
+      //case None => println("That didn't work.")
+      case None => "That didn't work."
+    }
+
+    println("result: " + result)
+    val stringA: String = "Hello"
+    val stringB: String = "9"
+    val stringC: String = "65535"
+
+
+    val y = for {
+      a <- toInt(stringA)
+      b <- toInt(stringB)
+      c <- toInt(stringC)
+    } yield b
+
+
+    println("a: " + y)
+
+    /*
+      val valueFromComputation = computeAndGetValueFor(someInput)
+      if (valueFromComputation ! = null) {
+        // do something with this value
+      }
+    */
 
 }
